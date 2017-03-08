3c255def4c8a43cdc2ee13a126f34e069360d235
Olivia <olivia.simmons@eagles.ewu.edu>
2017-03-08 14:53:16 -0800

    Getting Started!

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 // Store & Generate Number
 var maxRand = 3;
-var numRand = Math.floor( Math.random() * maxRand );
+var numRand = Math.floor(Math.random() * maxRand);
 //console.log(numRand);
 
 // Store images
@@ -15,5 +15,7 @@ $('header img').attr('src', eval('img' + numRand) );
 
 // Activate Gallery
 $('figure').lightGallery({
-    selector: 'a'
+    selector: 'a',
+    thumbnail: true,
 });
+
